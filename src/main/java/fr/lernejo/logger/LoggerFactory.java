24eb3a9cfe81584f1f66374617cf884a4680442d
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-01 22:38:43 +0100

    FilteredLogger, last commit

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -7,7 +7,10 @@ public class LoggerFactory {
 	public static @NotNull Logger getLogger(Class<?> callerClass, String name) {
 		return new ContextualLogger(callerClass, new CompositeLogger(
 			new ConsoleLogger(),
-			new FileLogger(System.getProperty("user.home") + "/Desktop/file.txt")
+			new FilteredLogger(
+				new FileLogger(System.getProperty("user.home") + "/Desktop/file.txt"),
+				s -> s.contains("Simulation")
+			)
 		));
 	}
 }
