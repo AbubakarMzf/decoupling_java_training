3de2f960e877309b37e8d01fba46d16b11f13bdd
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-01 14:39:42 +0100

    Add Player classes and some fix

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,12 @@
 package fr.lernejo.logger;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public class LoggerFactory {
 
-	public static Logger getLogger(String name) {
-		return null;
+	@Contract(value = "_ -> new", pure = true)
+	public static @NotNull Logger getLogger(String name) {
+		return new ConsoleLogger();
 	}
 }
