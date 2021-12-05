e47fb35b24474304660c4ad8408542516eba7449
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-05 20:54:07 +0100

    More fixes

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -32,11 +32,11 @@ public class Simulation {
 		if (guess == numberToGuess)
 			return true;
 		if (guess > numberToGuess) {
-			if (DEBUG) logger.log("C'est moins !");
+			if (player.getClass() == HumanPlayer.class || (player.getClass() == ComputerPlayer.class && DEBUG)) logger.log("C'est moins ! Entrez un nombre plus petit");
 			player.respond(false);
 		} else {
 			if (guess > 0) {
-				if (DEBUG) logger.log("C'est plus !");
+				if (player.getClass() == HumanPlayer.class || (player.getClass() == ComputerPlayer.class && DEBUG)) logger.log("C'est plus ! Entrez un nombre plus grand");
 				player.respond(true);
 			}
 		}
@@ -53,7 +53,7 @@ public class Simulation {
 			}
 		}
 		long elapsedTime = System.currentTimeMillis() - start;
-		logger.log(won ? "Félicitations, vous avez gagné !" : "Vous avez perdu !");
+		logger.log(won ? "Félicitations, vous avez gagné !" : "C'est la fin, vous avez perdu !");
 		logger.log("Temps total : " + new SimpleDateFormat("mm:ss.SSS").format(new Date(elapsedTime)));
 	}
 }
